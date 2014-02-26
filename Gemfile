70a187caafebde6faab4eb57ccbbb6fca0ff334c
sarah@spoonuniversity.com <sadler@u.northwestern.edu>
2014-02-26 09:18:15 -0500

    added devise and rspec

--- Gemfile
+++ Gemfile
@@ -24,12 +24,19 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
+gem 'devise'
+
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0.0.beta'
 end
 
 # Use ActiveModel has_secure_password
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,11 @@
 SecretSauce::Application.configure do
+
+
+  ## change local host to real host in production
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -34,6 +41,8 @@ SecretSauce::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+
+
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
