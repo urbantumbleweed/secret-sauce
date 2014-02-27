1a1760633620066029124fe77792c7021796b9b1
sarah@spoonuniversity.com <sadler@u.northwestern.edu>
2014-02-27 09:51:58 -0500

    mostly working

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -26,8 +26,9 @@ class PositionsController < ApplicationController
 		position = Position.find(params[:position])
 		if current_user.position == position
 			unless current_user.status.pages.include?(page)
-				current_user.status.pages =  current_user.status.pages + [page]
-					# THIS ISNT WORKING
+				current_user.status.pages += [page]
+				current_user.status.pages_will_change!
+				binding.pry
 			end
 		end
 		page = position.next_page(page)
