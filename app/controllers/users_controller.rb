c16059887fb1fc0affef6a4daac7e8f90c4f9eb8
kinzin <yniznik@gmail.com>
2014-02-27 12:52:19 -0500

    merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,6 @@ class UsersController < ApplicationController
 		before_filter :authenticate_user!
 
 	def home
-		@user = current_user
 	end
-
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :active, :school, :email)
-  end
-
 
 end
