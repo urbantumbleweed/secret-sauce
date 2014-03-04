cecf808cf1bc7960fecb2fe83ce460f118a975d8
sarah@spoonuniversity.com <sadler@u.northwestern.edu>
2014-03-04 11:50:23 -0500

    added config info to production.rb in an attempt to get images to appear on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,11 @@ SecretSauce::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+
+
 end
